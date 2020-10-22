e8ed56450d7997750b7b12833c167af2738432a4
Wisman Guevara <43938006+Wiz095@users.noreply.github.com>
2020-10-22 09:30:40 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Wildland Belize Adventures</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="HandheldFriendly" content="True">
+	<meta name="apple-mobile-web-app-status-bar-style" content="white">
+
+	<link rel="icon" href="img/main.jpg" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" media="all" href="css/default.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="css/home.css" />
+	<link rel="stylesheet" type="text/css" media="all" href="css/filter-gallery.css" />
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+
+
+</head>
+
+<body>
+	<div class="container">
+		<header class="slide">
+			<ul id="navToggle" class="burger slide">
+				<li class="line1"></li>
+				<li class="line2"></li>
+				<li class="line3"></li>
+			</ul>
+			<span class="h1" data-aos="zoom-in-right" data-aos-duration="1000" data-aos-delay="500">Wildland
+				Belize Adventures</span>
+			<dpan class="header-icon"><img src="img/main.jpg" alt=""></span>
+		</header>
+		<nav class="slide">
+			<ul data-aos="fade-up" data-aos-anchor-placement="bottom-center" data-aos-duration="1000">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="gallery.html">Gallery</a></li>
+				<li><a href="pricing.html">Pricing</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="contact.html">Contact</a></li>
+				<li><span class="header-btn-cont">
+						<div class="btn header-btn">
+							<strong>Book A Tour</strong>
+						</div>
+					</span>
+				</li>
+			</ul>
+		</nav>
+	</div>
+	<div class="container">
+		<div class="content slide">
+			<section id="banner">
+				<div class="banner-section">
+					<div class="placefiller">
+						<div class="banner-title">
+							<h1 data-aos="fade-right" data-aos-duration="700">A Portal to Nature's Wonders!</h1>
+							<h4 data-aos="fade-right" data-aos-duration="1000" data-aos-delay="200">Lorem ipsum dolori <br/>
+								sit, amet consectetur adipisicing elit.
+								<br>exercitationem, inventore aspernatur cum.
+							</h4>
+							<div id="banner-btn" class="banner-btn" data-aos="zoom-in" data-aos-duration="2000">
+								<span>Book a Tour</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="about">
+				<div class="placefiller">
+					<div class="about-us">
+						<div class="text-cont">
+							<div class="text-cont2" data-aos-duration="zoom-in" data-aos-duration="1000"
+								data-aos-delay="500">
+								<div class="title1" data-aos="zoom-in" data-aos-duration="500">
+									<span>
+										Who we are
+									</span>
+								</div>
+								<div data-aos="fade-left" data-aos-duration="500" data-aos-delay="100">
+									<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+										Recusandae, asperiores aut architecto veniam hic ea cumque debitis,
+										magni ad error maxime culpa eius! Impedit, laudantium atque numquam in amet
+										cupiditate? Lorem ipsum, dolor sit amet consectetur </p>
+								</div>
+							</div>
+							<div class="btn about-btn" data-aos="zoom-in-right" data-aos-duration="500">
+								<strong>
+									Learn More
+								</strong>
+							</div>
+						</div>
+						<div data-aos="zoom-in" data-aos-duration="800" data-aos-delay="500" class="img-cont">
+							<img src="img/ab.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="tours">
+				<div id="tours" class="tours">
+					<div class="tour-title" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500">
+						<span class="title" data-aos="fade-left" data-aos-duration="600">
+							Our Best Services
+						</span>
+					</div>
+					<div class="cards">
+						<div class="card" data-aos="fade-up" data-aos-duration="500">
+							<div>
+								<img src="img/14-1.jpg">
+								<h1>Cave Tubing</h1>
+								<p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Veniam unde delectus tempora
+								</p>
+								<div class="btn">
+									<strong>
+										Learn More
+									</strong>
+								</div>
+							</div>
+						</div>
+						<div class="card" data-aos="fade-up" data-aos-duration="800" data-aos-delay="100">
+							<div>
+								<img src="img/15.jpg" alt="">
+								<h1>Cave Exploring</h1>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Veniam unde delectus tempora
+								</p>
+								<div class="btn">
+									<strong>
+										Learn More
+									</strong>
+								</div>
+							</div>
+						</div>
+						<div class="card" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
+							<div>
+								<img src="img/11.jpg" alt="">
+								<h1>
+									Cave Kayaking</h1>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Veniam unde delectus tempora
+								</p>
+								<div class="btn">
+									<strong>
+										Learn More
+									</strong>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="hide-showcards">
+						<div class="cont-cards">
+							<div class="cards">
+								<div class="card" data-aos="fade-up" data-aos-duration="800">
+									<div>
+										<img src="img/p4.jpg" alt="">
+										<h1>
+											Bird Watching</h1>
+										<p>
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Veniam unde delectus tempora
+										</p>
+										<div class="btn">
+											<strong>
+												Learn More
+											</strong>
+										</div>
+									</div>
+								</div>
+								<div class="card" data-aos="fade-up" data-aos-duration="800">
+									<div>
+										<img src="img/pic03.jpg" alt="">
+										<h1>
+											Maya Ruins</h1>
+										<p>
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.dddaadsdszdexrqxrqe
+										</p>
+										<div class="btn">
+											<strong>
+												Learn More
+											</strong>
+										</div>
+									</div>
+								</div>
+								<div class="card" data-aos="fade-up" data-aos-duration="800">
+									<div>
+										<img src="img/banner9.jpg" alt="">
+										<h1>
+											Belize Zoo</h1>
+										<p>
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Veniam unde delectus tempora .
+										</p>
+										<div class="btn">
+											<strong>
+												Learn More
+											</strong>
+										</div>
+									</div>
+								</div>
+								<div class="card" data-aos="fade-up" data-aos-duration="800">
+									<div>
+										<img src="img/banner5.jpeg" alt="">
+										<h1>
+											Haunted Forest</h1>
+										<p>
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Veniam unde delectus tempora
+										</p>
+										<div class="btn">
+											<strong>
+												Learn More
+											</strong>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="display-card">
+							<div class="card-btn">View More</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="gallery">
+				<div class="gal-container">
+					<h1 class="gallery-title" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="500">
+						Recent Tours
+					</h1>
+					<div class="slider" data-aos="zoom-in" data-aos-duration="1000">
+						<div class="slide-items"><img src="img/2.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/3.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/4.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/5.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/7.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/8.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/9.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/10.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/11.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/12.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/13.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/14.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/15.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/16.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/17.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/18.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/19.jpg" alt="images"></div>
+						<div class="slide-items"><img src="img/22.jpg" alt="images"></div>
+					</div>
+				</div>
+			</section>
+			<footer id="">
+			  <div class="quick-links">
+			    <ul>
+			      <li><a href="about.html">What we do</a></li>
+			      <li><a href="price.html">Pricing</a></li>
+			      <li><a href="contact.html">Contact</a></li>
+			      <li><a href="service.html">Services</a></li>
+			    </ul>
+			  </div>
+			  <div class="social-media">
+			    <ul class="media-links">
+			      <li>
+			          <ion-icon name="logo-facebook"></ion-icon>
+			      </li>
+			      
+			      <li>
+			          <ion-icon name="logo-instagram"></ion-icon>
+			      </li>
+			      
+			       <li>
+			          <ion-icon name="logo-google"></ion-icon>
+			      </li>
+			    </ul>
+			  </div>
+			  <div class="contacts">
+			      <span class="location">
+			        Mile 32,Hummingbird Highway, Cayo District
+			      </span>
+			      
+              <span class="mail">
+                wildlandbelize@gmail.com
+              </span>
+			  </div>
+			  <div class="copy-right">
+			    <h6>
+			    Copyright &copy;<script>document.write(new Date().getFullYear());</script> Dotslash.All rights reserved.</h6>
+			  </div>
+			</footer>
+		</div>
+	</div>
+	<div class="modal">
+		<div class="content">
+			<div class="modal-content">
+				<div class="bg-img">
+					<img src="img/8.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="modal-title">
+						<h1>Book Your Tour</h1>
+						<div>
+							<p>Lorem ipsum dolor sit aanimi iure ut voluptate ducimus ipsa sed.
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+								Harum voluptatum blanditiis hic veritatis! Nam ratione,
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="form-container">
+				  <span class="form-title">Book You Tour</span>
+					<form action="mail.php" method="post" id="input-container" enctype="multipart/form-data">
+					  
+						<div>
+						<input id="userName" type="text" name="name" placeholder="Name" />
+						<label id="userName-info"></label>
+						</div>
+						
+					  <div>
+					   	<input type="email" id="userEmail" placeholder="Email" name="email">
+					   	<label id="userEmail-info"></label>
+					  </div>
+						
+						  <div>
+						  <input type="date"
+						   id="userDate" placeholder="MM/DD/YYYY" onfocus="(this.type='date')">
+						   <label id="userDate-info"></label>
+						  </div>
+						   
+						<select name="userNum" id="userNum">
+						  <option value="" disabled selected hidden> Choose Number Of People On Tour</option>
+						  <option value="1 Person">1 Person</option>
+						  <option value="2 Person"> 2 Person</option>
+						  <option value="3 Person">3 Person</option>
+						  <option value="4 Person">4 Person</option>
+						  <option value="5 Person">5 Person</option>
+						  <option value="6 Person">6 Person</option>
+						  <option value="7 Person">7 Person</option>
+						  <option value="8 Person">8 Person</option>
+						</select>
+						
+						<select name="tour" id="UserTour">
+						  <option value="" disables selected hidden>Choose Tour</option>
+							<option value="Kayaking">Kayaking</option>
+							<option value="Cave Tubbing">Cave Tubbing</option>
+							<option value="Cave Exploring">Cave Exploring</option>
+							<option value="Crystal Cave">Crystal Cave</option>
+							<option value="Bird Watching">Bird Watching</option>
+							<option value="Belize Zoo">Belize Zoo</option>
+							<option value="Haunted Forest">Haunted Forest</option>
+						</select>
+						<div class="close">Cancel</div>
+			      <button class="submit-btn" name="submit" type="submit">Book Now</button>
+			      
+			      <div id="statusMessage"> 
+                        <?php
+                        if (!empty($message)) {
+                          ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Scripts -->
+	<!-- <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script> -->
+	<script src="js/jquery-1.9.js"></script>
+	
+	<script type="text/javascript">
+  
+    function validateBookForm(){
+      var valid = true;
+      var userName = $("#userName").val();
+      var userEmail = $("#userEmail").val();
+      var subject = "Booked Tour";
+      var content = null;
+      var userDate = $("#userDate").datepicker('getDate');
+      var userNum = $("#UserNum").val();
+    
+      if (userName == "") {
+          $("#userName-info").html("Required.");
+          $("#userName").css('border', '#e66262 1px solid');
+                valid = false;
+      }
+      if (userEmail == "") {
+        $("#userEmail-info").html("Required.");
+        $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+      }
+      if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+      {
+        $("#userEmail-info").html("Invalid Email Address.");
+        $("#userEmail").css('border', '#e66262 1px solid');
+        valid = false;
+      }
+      if (userDate == "") {
+        $("#userDate-info").html("Required.");
+        $("#userDate").css('border', '#e66262 1px solid');
+        valid = false;
+      }
+            
+      content = "Name:" + userName + "" + "Date:" + userDate + "" + "Tour:" + userTour
+            
+      return valid;
+    }
+  </script>
+	
+	<script src="https://unpkg.com/ionicons@5.2.1/dist/ionicons.js"></script>
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript" src="js/formvalidation.js"></script>
+	<script>
+		AOS.init();
+	</script>
+</body>
+</html>
---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
+
+	$toEmail = "wisman.tg@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your tour was booked successfully.";
+	    $type = "success";
+	}
+}
+require_once "index.php";
+?>
